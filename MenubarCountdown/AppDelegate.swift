c2703ea9252d3cc11eba4abf30b374a4e53a6760
Kristopher Johnson <kris@kristopherjohnson.net>
2015-11-06 20:23:06 -0500

    Initial Commit

---
+++ MenubarCountdown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenubarCountdown
+//
+//  Created by Kristopher Johnson on 11/6/15.
+//  Copyright © 2015 Kristopher Johnson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenubarCountdown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenubarCountdown
+//
+//  Created by Kristopher Johnson on 11/6/15.
+//  Copyright © 2015 Kristopher Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
