38270fa705446e9c2c852064cf85988a36cf5cb7
Kristopher Johnson <kris@kristopherjohnson.net>
2015-11-08 13:52:09 -0500

    Add README and GPL

--- MenubarCountdown/CALayerExtensions.swift
+++ MenubarCountdown/CALayerExtensions.swift
@@ -2,8 +2,22 @@
 //  CALayerExtensions.swift
 //  MenubarCountdown
 //
-//  Created by Kristopher Johnson on 11/7/15.
 //  Copyright © 2015 Kristopher Johnson. All rights reserved.
+//
+//  This file is part of Menubar Countdown.
+//
+//  Menubar Countdown is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Menubar Countdown is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Menubar Countdown.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import Cocoa
