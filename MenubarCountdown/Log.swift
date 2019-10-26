478c08860d650d4420ebab15433e2682f1cfc315
Kristopher Johnson <kris@kristopherjohnson.net>
2019-10-26 10:56:30 -0400

    Use os_log instead of NSLog for logging

--- MenubarCountdown/Log.swift
+++ MenubarCountdown/Log.swift
@@ -5,32 +5,59 @@
 //  Copyright © 2009,2015,2019 Kristopher Johnson. All rights reserved.
 
 import Foundation
+import os.log
 
+/**
+ Application logging functions.
+
+ All app components should call these functions instead of using `NSLog()` or `os_log()`.
+ */
 struct Log {
 
-    /// Log an error message
+    /**
+     Log an error message.
+
+     - parameters:
+        - message: Text to be logged.
+        - function: Name of calling function.
+        - file: Source file name of calling function.
+        - line: Source line number of call location.
+     */
     static func error(_ message: String,
                       function: String = #function, file: String = #file, line: Int32 = #line)
     {
         let filename = file.lastPathComponent
 
         let msg = "ERROR: \(message) [\(function) \(filename):\(line)]"
-        NSLog("%@", msg);
+        os_log("%{private}@", type: .error, msg)
 
         DTraceErrorMessage(message, function, filename, line)
     }
 
-    /// Log a debug-level message
+    /**
+     Log a debug-level message.
+
+     Has no effect in a Release build.
+
+     - parameters:
+        - message: Text to be logged.
+        - function: Name of calling function.
+        - file: Source file name of calling function.
+        - line: Source line number of call location.
+     */
     static func debug(_ message: String,
                       function: String = #function, file: String = #file, line: Int32 = #line)
     {
-        #if DEBUG
-            let filename = file.lastPathComponent
-            
-            let msg = "DEBUG: \(message) [\(function) \(filename):\(line)]"
-            NSLog("%@", msg);
+#if DEBUG
+        let filename = file.lastPathComponent
 
-            DTraceDebugMessage(message, function, filename, line)
-        #endif
+        let msg = "DEBUG: \(message) [\(function) \(filename):\(line)]"
+
+        // Note: we log using default level rather than .debug so that the
+        // messages always go into the log for a debug build
+        os_log("%{private}@", msg)
+
+        DTraceDebugMessage(message, function, filename, line)
+#endif
     }
 }
